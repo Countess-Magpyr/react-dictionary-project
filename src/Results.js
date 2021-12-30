d3c4bb984b1b4751f61fd73af4fe7940b7fcd608
Countess-Magpyr <91279492+Countess-Magpyr@users.noreply.github.com>
2021-12-30 17:22:42 +0100

    Added Meaning component

--- src/Results.js
+++ src/Results.js
@@ -7,6 +7,9 @@ export default function Results(props) {
     return (
       <div className="Results">
         <h2>{props.results.word}</h2>
+        <h4>Pronounciation: {props.results.phonetic}</h4>
+        <h6>Origin: {props.results.origin}</h6>
+
         {props.results.meanings.map(function (meaning, index) {
           return (
             <div key={index}>
