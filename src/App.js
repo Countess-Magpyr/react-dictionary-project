200c2d4bfbf13e85dd91391d9e9ff23cc61ea510
Countess-Magpyr <91279492+Countess-Magpyr@users.noreply.github.com>
2021-12-30 13:32:16 +0100

    form added to dictionary

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <div className="container">Dictionary</div>
+      <div className="container">
+        <header className="App-header">Welcome to my Dictionary</header>
+        <Dictionary />
+      </div>
+      <footer className="App-footer">
+        <small>This app was made by SugarMouse</small>
+      </footer>
     </div>
   );
 }
---
+++ src/Dictionary.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import "./Dictionary.css";
+
+export default function Dictionary() {
+  let [keyword, setKeyword] = useState("");
+  function search(event) {
+    event.preventDefault();
+    alert("Searching");
+  }
+  return (
+    <div className="Dictionary">
+      <form onSubmit={search}>
+        <input type="search" autofocus={true} />
+      </form>
+    </div>
+  );
+}
