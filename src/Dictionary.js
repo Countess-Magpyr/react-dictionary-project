95a93f11f27aef9de3959094ed29fd4680852009
Countess-Magpyr <91279492+Countess-Magpyr@users.noreply.github.com>
2021-12-30 21:01:45 +0100

    photos

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,54 +1,71 @@
 import React, { useState } from "react";
-import Results from "./Results";
-import "./Dictionary.css";
 import axios from "axios";
+import Results from "./Results";
+import Photos from "./Photos";
+import "./Dictionary.css";
 
-export default function Dictionary() {
-  let [keyword, setKeyword] = useState("");
+export default function Dictionary(props) {
+  let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
+  let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
 
-  function handleResponse(response) {
-    console.log(response.data[0].meanings[0].definitions[0].definition);
+  function handleDictionResponse(response) {
     setResults(response.data[0]);
   }
 
   function handlePexelsResponse(response) {
-    console.log(response.data[0].meanings[0].definitions[0].definition);
-    setResults(response.data[0]);
+    setPhotos(response.data.photos);
   }
 
-  function search(event) {
-    event.preventDefault();
-
-    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-    console.log(apiUrl);
-    axios.get(apiUrl).then(handleResponse);
+  function search() {
+    // documentation: https://dictionaryapi.dev/e
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+    axios.get(apiUrl).then(handleDictionResponse);
 
     let pexelsApiKey =
       "563492ad6f917000010000014a5eba819f90446b82316c7fb94a479a";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+    let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
+  }
 
-    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=6`;
-
-    axios
-      .get(pexelsApiUrl, {
-        headers: { Authorization: `Bearer${pexelsApiKey}` },
-      })
-      .then(handlePexelsResponse);
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
   }
 
   function handleKeywordChange(event) {
-    console.log(event.target.value);
     setKeyword(event.target.value);
   }
-  return (
-    <section>
+
+  function load() {
+    setLoaded(true);
+    search();
+  }
+
+  if (loaded) {
+    return (
       <div className="Dictionary">
-        <h5>What word would you like to find? </h5>
-        <form onSubmit={search}>
-          <input type="search" onChange={handleKeywordChange} />
-        </form>
+        <section>
+          <h1>What word do you want to look up?</h1>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="search"
+              onChange={handleKeywordChange}
+              defaultValue={props.defaultKeyword}
+            />
+          </form>
+          <div className="hint">
+            suggested words: sunset, wine, yoga, plant...
+          </div>
+        </section>
         <Results results={results} />
+        <Photos photos={photos} />
       </div>
-    </section>
-  );
+    );
+  } else {
+    load();
+    return "Loading";
+  }
 }
